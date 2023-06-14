6c5a326967325a53c425a8a0d69009738c2cd263
McStudent01 <mcmillcy@mail.uc.edu>
2023-06-14 13:47:59 -0500

    Initial commit

---
+++ src/Ship_Cost_Calculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ship_Cost_Calculator
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double SHIPPING_RATE = 0.02;
+        double itemCost = 0;
+
+        System.out.println("What is the item cost: ");
+        itemCost = in.nextDouble();
+
+        double shippingCost;
+        if (itemCost >= 100)
+            {
+                shippingCost = 0;
+            } else {
+            shippingCost = itemCost * SHIPPING_RATE;
+        }
+        double totalCost = itemCost + shippingCost;
+
+        System.out.println("The shipping cost is $" + shippingCost);
+        System.out.println("The total cost is $" + totalCost);
+
+    }
+}
